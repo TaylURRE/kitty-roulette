e8a5d4c63aa8a767b8b797db34e7151b63fc10da
Taylor Stallworth <tstall17@gmail.com>
2017-09-11 12:44:54 -0500

    Add custom alert, and next button

--- scripts/main.js
+++ scripts/main.js
@@ -25,17 +25,54 @@
 // Later
 // lives === 0
 
-/*jslint devel: true */
+// lives
+// Attack(Play 3 times in a row)
+// Skip
+// Play
 
-var beginScenario = ["Bubbles, is on the brink of a break down(kicking kitty litter everywhere)",
-        "Fluffy, is threatening to spray the place up", "Jinx, wants to bite every customer in the place"];
+/*jslint devel: true */
+/*global document: false */
+
+
+var catNames = ["Bubbles Cat", "Chuckles Cat", "Bob Cat", "Natasha Cat", "Boris Cat", "Fluffy Cat", "Smelly Cat" ];
+function nextPlay(currentPlay) {
+    "use strict";
+    document.getElementById("popBox").innerHTML = currentPlay;
+}
 
 function generateRandomNumber(numberRange) {
     "use strict";
     if (typeof numberRange === "number") {
-        return Math.floor(Math.random() * numberRange - 1);
+        return Math.floor(Math.random() * (numberRange - 1));
     }
 }
 
-alert("You're the {assign a task} Manager 2 Lives Barista 3 Lives Owner 1 Life of your local cat cafe, and the health inspector is visiting in 5mins. Your kittys are aware of the impending visit and all hell breaks loose!");
-alert("The ring leader, " + beginScenario[generateRandomNumber(beginScenario.length)]);
+var playerOne = {
+        name: prompt("What's your name?"),
+        lives: 9,
+        attackCard: 2,
+        skip: 1,
+        play: true
+    };
+
+var feralCat = {
+        name: catNames[0],
+        lives: 9,
+        attackCard: 1,
+        skip: 2,
+        play: false
+    };
+
+var beginScenario = [", is on the brink of a break down(kicking kitty litter everywhere)", ", is buzzed up on Cat Nip and saying they will poo in all the shoes",
+            ", is threatening to spray the place up", ", wants to bite every customer in the place"];
+
+
+nextPlay(playerOne.name + ", you're the Manager of your local cat cafe, and the health inspector is visiting in 5mins. Your kittys are aware of the impending visit and all hell breaks loose!" +
+    " The ring leader, " + feralCat.name + " " + beginScenario[generateRandomNumber(beginScenario.length)]);
+document.getElementById("nextButton").onclick = function () {
+    'use strict';
+    nextPlay("Schwiffty");
+};
+// alert(playerOne.name + ", you're the Manager of your local cat cafe, and the health inspector is visiting in 5mins. Your kittys are aware of the impending visit and all hell breaks loose!");
+// alert("The ring leader, " + feralCat.name + " " + beginScenario[generateRandomNumber(beginScenario.length)]);
+// alert(feralCat.name + ", Informs you the only way to save your cafe is to win against them in a game of Kitty Roulette");
