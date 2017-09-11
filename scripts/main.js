6ee5f723f21b6e9c83a8c6ed45d2ecb065aa1787
Taylor Stallworth <tstall17@gmail.com>
2017-09-11 11:27:59 -0500

    Intial Commit

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+// Kitty Roulette
+//
+// You're the manager of your local cat cafe, and the health inspector is visiting in 5mins.
+// Your kittys are aware and all hell breaks loose!
+//
+// The ring leader,
+//
+// Bubbles, is on the brink of a break down(kicking kitty litter everywhere)
+// Fluffy, is threatening to spray the place up
+// Jinx, wants to bite every customer in the place
+//
+//
+// Only you can save the cafe, they challenge you to a game of kitty roulette
+//
+// If you win you take the place of the ring leader. You also keep your dignity and Cafe
+// If you lose you will fail health inspection and loose the Cat Cafe
+//
+// 9 lives
+//
+//
+// Losing Scenario
+// for Now
+// ScenarioNumber === 0
+//
+// Later
+// lives === 0
+var beginScenario = ["Bubbles, is on the brink of a break down(kicking kitty litter everywhere)",
+        "Fluffy, is threatening to spray the place up", "Jinx, wants to bite every customer in the place"];
+
+function generateRandomNumber(numberRange) {
+    "use strict";
+    if (typeof numberRange === "number") {
+        return Math.floor(Math.random() * numberRange - 1);
+    }
+}
