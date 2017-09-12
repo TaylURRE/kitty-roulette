916a45554bbfa1ac0d1643587148ed66f0dda35a
Taylor Stallworth <tstall17@gmail.com>
2017-09-12 16:25:47 -0500

    Fix typos

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,14 @@
 /*jslint devel: true */
 /*global document: false */
 
-
 var catNames = ["Bubbles Cat", "Chuckles Cat", "Bob Cat", "Natasha Cat", "Boris Cat", "Fluffy Cat", "Smelly Cat" ];
-var beginScenario = [", is on the brink of kicking kitty litter everywhere", ", is buzzed up on Cat Nip and saying she will poo in all the shoes",
-            ", is threatening to spray the place up", ", wants to bite every customer in the place", " informs you they will steal thumbtacks from corkboards and put them all the shoes when we're not looking."];
+var beginScenario = [", is on the brink of kicking kitty litter everywhere", ", is buzzed up on Cat Nip and saying they will poo in all the shoes",
+            ", is threatening to spray the place up.", ", wants to bite every customer in the place", " informs you they will steal thumbtacks from corkboards and put them all the shoes when we're not looking."];
 var playScenario = ["extends one claw and drags it across their throat to signal the game will be over soon. They play five finger fillet with their sharpest claw and your fingers. You are shaking and afraid of the outcome this might be it for you...",
                     "starts to look at you pleadingly, paces around, and meows insistently. Your ears begin to curdle, its a horrible sound...",
-                    "farts like a grown man. They call it death by 1000 litter boxes...the smell chokes your and causes your right eye to twitch...",
-                    "pulls a Big RED button from their poofy coat while they look dead into your eyes they press the button. A coil from the $5000 espresso machine pops out and flings towards you...",
-                    "licks your hand and eyeballs unaware of if your are immune to TOXOPLASMOSIS turns out that you aren't"];
+                    "farts like a grown man. They call it death by 1000 litter boxes...the smell chokes you and causes your right eye to twitch...",
+                    "pulls a BIG RED button from their poofy coat while they look dead into your eyes they press the button. A coil from the $5000 espresso machine pops out and flings towards you...",
+                    "licks your hand and eyeballs. The goal, TOXOPLASMOSIS, unaware of if you are immune you hope for the best as you begin to sweat..."];
 function generateRandomNumber(numberRange) {
     "use strict";
     if (typeof numberRange === "number") {
@@ -64,7 +63,7 @@ function nextPlay(currentPlayScenario) {
             console.log("You can continue to live");
         }
     } else if (round > 5) {
-        addToPage("outcome", "Game OVER");
+        addToPage("outcome", "GAME OVER");
 
     }
     addToPage("popBox", "<p>" + currentPlayScenario + "</p>");
@@ -76,9 +75,9 @@ function updateScores() {
     addToPage("playerOneScore", playerOne.name + "'s Score: " + playerOne.lives + "</br> Round:" + round);
 }
 
-nextPlay("<p>" + playerOne.name + ", you're the Manager of your local cat cafe, Catopia, and the health inspector is visiting today. Your kittys are aware of the impending visit and all hell breaks loose!</p>" +
+nextPlay("<p>" + playerOne.name + ", you're the Manager of your local cat cafe, Catopia. The health inspector is visiting today, your kittys are made aware of the impending visit and all hell breaks loose!</p>" +
           "<p>The ring leader, " + feralCat.name + " " + beginScenario[generateRandomNumber(beginScenario.length)] + " causing a true Catastrophe.</p>" +
-          "<p>" + feralCat.name + ", Informs you the only way to save your cafe is to beat them in a game of Kitty Roulette</p>" +
+          "<p>" + feralCat.name + ", Informs you the only way to save your cafe is to beat them in a game of Kitty Roulette.</p>" +
           "<p>Normally they play Kitty Roulette with 9 lives but because of your feeble human body they are only giving you <b>3 Lives</b></p><p>" +
           feralCat.name + " will put you through a series or random situations known to cause pain to humans. If you survive <b>5 Rounds</b> unharmed you Win...If you don't you will meet your demise and lose your job </p>", 0);
 
